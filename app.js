a4fa1f8e3a6a0d1432da942d24af83148ac46f00
wilcorfis <wilmerandres505@gmail.com>
2021-12-09 07:56:24 -0500

    enviando id por jwt

---
+++ app.js
@@ -0,0 +1,44 @@
+const { ApolloServer } = require('apollo-server-express');
+const express = require('express');
+const typeDefs = require('./graphQL/typeDefs');
+const resolvers = require('./graphQL/resolvers');
+const authRoute = require('./routes/auth.routes')
+const { validarToken, admin, estudiante, lider } = require('./middleware/authjwt')
+
+
+// realizar la conexión a la BD
+const bd = require('./infrastructure/bd');
+bd.conectar();
+
+const PUERTO = 3020;
+
+const iniciarServidor = async () => {
+    const app = express();
+    const apollo = new ApolloServer(
+        {
+            typeDefs,
+            resolvers
+        });
+    await apollo.start();
+    apollo.applyMiddleware({ app: app });
+    app.use(express.json())
+    app.use('/api', authRoute)
+
+    // ********* Dashboards ***************
+    app.get('/api/dashboard/admin', [validarToken, admin], (req, res) => {
+        res.json("Soy el dashboard del administrador")
+    })
+    app.get('/api/dashboard/estudiante', [validarToken, estudiante], (req, res) => {
+        res.json("Soy el dashboard del estudiante")
+    })
+    app.get('/api/dashboard/lider', [validarToken, lider], (req, res) => {
+        res.json("Soy el dashboard del lider")
+    })
+    // ************************************
+
+    app.listen(PUERTO, () => {
+        console.log(`Servicio iniciado a través de la url http://localhost:${PUERTO}`);
+    });
+}
+
+iniciarServidor()
